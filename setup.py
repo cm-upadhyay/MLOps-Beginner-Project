b239bacb14e383f9871dee1ff3e5721b5567a712
cm-upadhyay <upadhyay.chandramohan@gmail.com>
2025-03-21 16:32:35 +0530

    project structure and venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="CM upadhyay",
+    author_email="upadhyay.chandramohan@gmail.com",
+    packages=find_packages()
+)
